70c37c53bb8a9f8ca15197c45f88acdf75a19fb0
Mistium <d.a.kimber@icloud.com>
2026-01-03 02:08:05 +0000

    Build for 655e1c1ef3c9c9b8f9719666e77ea9280b3b8eda

---
+++ js/gleba/addon-entry-tw-remove-feedback.b3d5c3629da496dfb463.js
@@ -0,0 +1,1 @@
+(window.webpackJsonpGUI=window.webpackJsonpGUI||[]).push([[66],{1669:function(n,e,s){"use strict";s.r(e),s.d(e,"resources",(function(){return o}));var c=s(1670);const o={"remove-feedback.css":s.n(c).a}},1670:function(n,e,s){(n.exports=s(11)(!1)).push([n.i,'a[class^="menu-bar_feedback-link_"] {\n  display: none;\n}\n',""])}}]);
