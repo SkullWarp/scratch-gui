39786a8f7e5592cf1ebebeccaeb0e2c77c7558f1
Flufi-boi <floofster@flufi.gay>
2026-01-03 23:09:17 +0000

    Build for 246ce0139181be4b02efa85c9a33fe2c9b64c8e5

--- js/gleba/addon-entry-tw-remove-feedback.f8a2227a5cb2a50a4787.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonpGUI=window.webpackJsonpGUI||[]).push([[66],{1640:function(n,e,s){"use strict";s.r(e),s.d(e,"resources",(function(){return o}));var c=s(1641);const o={"remove-feedback.css":s.n(c).a}},1641:function(n,e,s){(n.exports=s(11)(!1)).push([n.i,'a[class^="menu-bar_feedback-link_"] {\n  display: none;\n}\n',""])}}]);
